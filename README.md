4e8371d99a0a1aec34b786453c8c82d5c5fde6c5
argenis0409 <argenis04_09@icloud.com>
2019-05-01 11:36:00 -0700

    astro weight calculator

---
+++ README.md
@@ -0,0 +1,8 @@
+Welcome to my Astro Weight Calculator.
+
+In this project you will be able to check how much you would weight in your favorite planet, if you had the oportunity to go to space.
+
+To see the project working is simple, copy the project files in your code editor then run npm start,
+once you do that, you will see an output looking like this >> "serving "." at http://127.0.0.1:8080" 
+
+Ok have fun and hope you like it.
--- main.js
+++ main.js
@@ -1,1 +1,48 @@
 // Write your JavaScript code here!
+var planets = [
+    ['Pluto', 0.06],
+    ['Neptune', 1.148],
+    ['Uranus', 0.917],
+    ['Saturn', 1.139],
+    ['Jupiter', 2.640],
+    ['Mars', 0.3895],
+    ['Moon', 0.1655],
+    ['Earth', 1],
+    ['Venus', 0.9032],
+    ['Mercury', 0.377],
+    ['Sun', 27.9]
+  ];
+  
+var planetsArr = planets.reverse();
+
+planetsArr.forEach(function(planet) {
+var array = document.createElement('option');
+array.text = planet[0];
+array.value = planet[0];
+document.getElementById('planets').appendChild(array);
+})
+
+  function calculateWeight(weight, planetName) {
+  for(let i = 0; i < planets.length; i++) {
+    if(planets[i][0] == planetName) {
+     var multiply = planets[i][1] * weight;
+    }
+  }
+  return multiply;
+  }
+  
+
+  function handleClickEvent(e) {
+    // 3. Create a variable called userWeight and assign the value of the user's weight.
+  var userWeight = $('#user-weight').val();
+    // 4. Create a variable called planetName and assign the name of the selected planet from the drop down.
+  var planetName = $('#planets').val();
+    // 5. Create a variable called result and assign the value of the new calculated weight.
+  var result = calculateWeight(userWeight, planetName);
+    // 6. Write code to display the message shown in the screenshot.
+  $('#output').text('If you were on ' + planetName +','+ ' you would weigh ' + result + 'lbs!');
+
+  }
+
+  $('#calculate-button').click(handleClickEvent);
+
